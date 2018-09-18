88f914b519d2df6d3989d971e1fa51a462a6f58b
gongwen yu <gongwenyu@gongwendeMac-mini.local>
2018-09-18 22:36:50 +0800

    现在可以切换代执行任务和已经执行任务了

--- main/js/main.js
+++ main/js/main.js
@@ -1,4 +1,4 @@
-
+//类名中含有nav的li 元素切换添加here类名
 $(document).ready(function(){
 	$('li.nav').click(function(){
 		$(this).addClass("here");
@@ -6,8 +6,7 @@ $(document).ready(function(){
 	})
 })
 
-//含有nav的li 元素切换添加here类名
-
+//点击全选的checkbox则下面的复选框都选中，
 $(function(){
 	$("input[name=all]").click(function(){
 		if (this.checked){
@@ -18,7 +17,17 @@ $(function(){
 	});
 });
 
-// 点击待执行和已执行按钮之间进行切换。
+
+
+//点击在待执行和已执行任务之间切换任务
 $(document).ready(function(){
-	$('.')
+	$('.done').click(function(){
+            $(".contentD").show();
+            $(".contentC").hide();
+	});
+	$('.ready').click(function(){
+            $(".contentC").show();
+            $(".contentD").hide();
+	});
+
 })
